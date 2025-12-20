515f92ccd63ea3e215a898e5924b97f67d68b32a
LMB-MBS <NIE25.LMB@e.ntu.edu.sg>
2025-12-21 01:19:03 +0800

    Fresh baseline after importing project on home laptop

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.abang.prayertimecompare"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.abang.prayertimecompare"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.88"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this
+            if (output is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
+                // Example: PrayerTimeCompare-1.0-release.apk
+                output.outputFileName = "${rootProject.name}-${variant.versionName}-${variant.buildType.name}.apk"
+            }
+        }
+    }
+
+}
+
+
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+// Custom task to push/copy APK to phone
+tasks.register("pushApkToPhone") {
+    dependsOn("assembleDebug")
+
+    doLast {
+        // Adjusted to your personalized APK name
+        val apkPath = file("$projectDir/build/outputs/apk/debug/PrayerTimeCompare-1.25-debug.apk")
+        val adbPath = "D:/Android/Sdk/platform-tools/adb.exe"
+
+        println("📦 Built APK: ${apkPath.absolutePath}")
+        if (apkPath.exists()) {
+            val result = project.exec {
+                commandLine(adbPath, "push", apkPath.absolutePath, "/storage/emulated/0/apk/${apkPath.name}")
+                isIgnoreExitValue = true
+            }
+            println("adb exit code: ${result.exitValue}")
+        } else {
+            println("⚠️ APK not found at ${apkPath.absolutePath}")
+        }
+    }
+}
+// Run automatically after build
+tasks.named("build").configure {
+    finalizedBy("pushApkToPhone")
+}
+
+
+
